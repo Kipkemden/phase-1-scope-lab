0bbf9bab4440899795a1ae0eae4297f8af26bda1
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-03-22 00:37:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+window.bestCustomer = undefined;
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+window.bestCustomer = undefined;
+function overwriteBestCustomer(newCustomer) {
+  window.bestCustomer = newCustomer;
+}
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not bob';
+}
